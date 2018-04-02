781af9cee0e8b254f7bc195fb6178fbef73a6c21
Alexander Urizar <urizaraj@gmail.com>
2018-04-02 16:56:10 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -8,33 +8,33 @@ class ArtistsController < ApplicationController
   # def new
   #   @artist = Artist.new
   # end
-  # 
+  #
   # def create
   #   @artist = Artist.new(artist_params)
-  # 
+  #
   #   if @artist.save
   #     redirect_to @artist
   #   else
   #     render :new
   #   end
   # end
-  # 
+  #
   # def edit
   #   @artist = Artist.find(params[:id])
   # end
-  # 
+  #
   # def update
   #   @artist = Artist.find(params[:id])
-  # 
+  #
   #   @artist.update(artist_params)
-  # 
+  #
   #   if @artist.save
   #     redirect_to @artist
   #   else
   #     render :edit
   #   end
   # end
-  # 
+  #
   # def destroy
   #   @artist = Artist.find(params[:id])
   #   @artist.destroy
